8ba04aa5a3f4b6225a39f5c74a807ad6c085bbe5
degawa09268 <72345408+degawa09268@users.noreply.github.com>
2020-10-15 07:36:37 +0900

    my  first website

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
 let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
@@ -9,4 +6,28 @@ myImage.onclick = function() {
     } else {
         myImage.setAttribute ('src', 'images/firefox-icon.png');
     }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is wonderful,' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is wonderful,' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
 }
